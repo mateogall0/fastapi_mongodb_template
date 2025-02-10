2f00124984f1ad99ab9afca19d41d600de4fe0a6
Mateo Gallo <92031297+mateogall0@users.noreply.github.com>
2025-02-10 08:53:42 -0300

    Update conn.py

--- app/conn.py
+++ app/conn.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-from motor.motor_asyncio import AsyncIOMotorClient
+from mongoengine import connect
 from app.config import settings
 
-client = AsyncIOMotorClient(settings.MONGO_URI)
-db = client[settings.DATABASE_NAME]
+connect(
+    host=settings.MONGO_URI,
+    uuidRepresentation='standard',
+)
+
+from mongoengine.connection import get_db
+
+db = get_db()
