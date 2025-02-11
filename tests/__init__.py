8f4f4ef1cbcda21a25294b670cf39ce1f7bbf523
Mateo Gallo <92031297+mateogall0@users.noreply.github.com>
2025-02-11 04:26:17 -0300

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 from fastapi.testclient import TestClient
 from app.main import app
+import pytest
+from app.conn import db
+from app.config import settings
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    client = TestClient(app)
+    yield client
+    db.client.drop_database(settings.DATABASE_NAME)
