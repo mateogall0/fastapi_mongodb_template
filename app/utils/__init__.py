6ffabdb9c1f0ec17be6e773b72f40b740c163fc4
mateogall0 <mateogesede@gmail.com>
2025-03-03 11:35:24 -0300

    Add utils

---
+++ app/utils/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from .doc import (
+    check_doc_input,
+    doc_existence,
+)
+from .debug import (
+    DEBUG,
+    DEBUG_ENV_NAMES,
+    timer,
+    print_debug
+)
---
+++ app/utils/debug.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from app.config import settings
+import time
+from inspect import currentframe, getfile
+from os import getenv
+
+DEBUG_ENV_NAMES = ['dev']
+DEBUG = settings.NAME in DEBUG_ENV_NAMES
+
+
+def timer(func):
+    """
+    Time measurer using this wrapper. Takes the time right before executing
+    the method and right after and substracts them to get the time it took to
+    execute the method.
+
+    Usage:
+        ```
+        @timer
+        def method(self, user):
+            pass
+        ```
+    """
+    def wrapper(*ag, **kw):
+        if not DEBUG:
+            return func(*ag, **kw)
+        start = time.time()
+        result = func(*ag, **kw)
+        end = time.time()
+        print(f"{func.__name__} took {end - start:.6f} seconds")
+        return result
+    return wrapper
+
+def print_debug(key='Print debug', value=None, separator='=',
+                end='\n\n') -> None:
+    """
+    Print big values of types like dictionaries with keys tp ease debugging
+    process.
+    """
+    if not DEBUG: return
+    frame = currentframe().f_back
+    file_name = getfile(frame)
+    line_number = frame.f_lineno
+    text = f'{key} at {file_name}:{line_number}'
+    print(text)
+    print(separator * len(text))
+    print(value, end=end)
