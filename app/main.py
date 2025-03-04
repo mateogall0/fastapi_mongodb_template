d0167f1d609e34269ba848a05f76173a2e69f11b
gallit0 <mateogesede@gmail.com>
2025-03-04 14:44:36 -0300

    Add middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
 from app.routes import init_routes
+from app.middleware import init_middleware
 import app.conn
 app = FastAPI()
 
 
 init_routes(app)
+init_middleware(app)
---
+++ app/middleware/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def init_middleware(app):
+    """
+    Add middleware implementations here
+    """
