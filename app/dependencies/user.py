e9a788d85a33f8d2a1358d036d20b5f280caf86a
gallit0 <mateogesede@gmail.com>
2025-03-05 10:44:39 -0300

    Improve flow

--- app/dependencies/user.py
+++ app/dependencies/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from fastapi import Depends
-from .token import get_user_from_token
 from . import security
 from app.utils import decode_payload
 from fastapi import HTTPException
