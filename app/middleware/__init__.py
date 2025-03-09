5d0261dfda8e2c0fb6d1b4b612e85ae29e62bb15
gallit0 <mateogesede@gmail.com>
2025-03-09 13:55:50 -0300

    Add cors

--- app/middleware/__init__.py
+++ app/middleware/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+from fastapi import FastAPI
+from .cors import args as cors_args
 
-def init_middleware(app):
-    """
-    Add middleware implementations here
-    """
+
+def init_middleware(app: FastAPI):
+    app.add_middleware(**cors_args)
--- app/middleware/cors.py
+++ app/middleware/cors.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+from fastapi.middleware.cors import CORSMiddleware
+import json
+def _read_origins(path='origins.json'):
+    with open(path, 'r') as f:
+        j = json.loads(f.read())
+        return j['allowed_origins']
+
+args = {
+    'middleware_class': CORSMiddleware,
+    'allow_origins': _read_origins(),
+    'allow_credentials': True,
+    'allow_methods': ['*'],
+    'allow_headers': ['*']
+}
