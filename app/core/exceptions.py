c0fc14baa72f7cb7f6609f1f42e7181a60d08eda
mateogall0 <mateogesede@gmail.com>
2025-04-28 09:26:53 -0300

    Add extra core exceptions

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -6,3 +6,23 @@ exception.
 """
 
 class CoreExceptions(Exception): pass
+
+class Conflict(CoreExceptions): pass
+
+class NotFound(CoreExceptions): pass
+
+class PreconditionFail(CoreExceptions): pass
+
+class Unexpected(CoreExceptions): pass
+
+class UserError(CoreExceptions): pass
+
+class Unauthorized(CoreExceptions): pass
+
+class NotEnoughAmount(CoreExceptions): pass
+
+class Timeout(CoreExceptions): pass
+
+class RateLimited(CoreExceptions): pass
+
+class Forbidden(CoreExceptions): pass
