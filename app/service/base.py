90069349ca1b4ed550610aa49a55d74d284aec7c
mateogall0 <mateogesede@gmail.com>
2025-06-16 13:42:42 -0300

    Add crud serice

---
+++ app/service/base.py
@@ -0,0 +1,22 @@
+from app.repository.base import BaseRepository
+
+
+class CRUDService:
+    def __init__(self, repo: BaseRepository):
+        self.repo: BaseRepository = repo
+
+    async def create(self, data: dict):
+        return await self.repo.create(data)
+
+    async def get(self, id: str):
+        return await self.repo.get_by_id(id)
+
+    async def get_many(self, skip=0, limit=100, filters={}):
+        return await self.repo.get_many(_ignore_none=True, **filters)[skip:skip + limit]
+
+    async def update(self, obj, data: dict):
+        return await self.repo.update(obj, data)
+
+    async def delete(self, obj):
+        return await self.repo.delete(obj)
+
