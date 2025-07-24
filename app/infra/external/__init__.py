195f43be17556c213f1204edad35c781c33373a5
mateogall0 <mateogesede@gmail.com>
2025-07-24 13:52:45 -0300

    Remove unused bita submodule

--- app/infra/external/__init__.py
+++ app/infra/external/__init__.py
@@ -1,12 +1,3 @@
 """
 External functionallities.
 """
-from app.infra.external.bita.sdk import BitaSDK
-from app.infra.config import settings
-from os import getenv
-from app.utils import TEST
-
-if getenv('BITA_SDK_INITAL_ACCESS_TOKEN') is None or TEST:
-    bita = None
-else:
-    bita = BitaSDK(host=settings.BITA_GATEWAY_HOST)
