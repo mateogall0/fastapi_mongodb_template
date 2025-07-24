2eab9809085dded4b83dea4015188eccebb15797
mateogall0 <mateogesede@gmail.com>
2025-07-24 13:42:58 -0300

    Remove unused socket logic

--- tests/unit/service/test_base.py
+++ tests/unit/service/test_base.py
@@ -1,5 +1,5 @@
 from tests import db
-from app.service.base import CRUDService as MongoService, SocketRequestService
+from app.service.base import CRUDService as MongoService
 import pytest
 
 
@@ -34,28 +34,3 @@ async def test_crud0(db):
 
     check_stored = await service.get(id)
     assert check_stored is None
-
-@pytest.mark.asyncio
-async def test_ws():
-    class Example(SocketRequestService):
-        async def test_req(self, payload: dict):
-            return {'res': payload['num'] * 2}
-    sr = Example()
-
-    res = await sr.handshake()
-    assert res == {'status': 'connected'}
-
-    res = await sr.handle('test_req', {'num': 5})
-    assert res == {'res': 10}
-
-    res = await sr.handle('test_req', {'num': 15})
-    assert res == {'res': 30}
-
-    res = await sr.handle('ping', {})
-    assert res == {'status': 'pong'}
-
-    res = await sr.handle('unexisting', {})
-    assert res == {'error': 'unknown action `unexisting`'}
-
-    res = await sr.handle('handle', {})
-    assert res == {'error': 'wrong action type'}
