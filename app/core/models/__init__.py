9aca6ba75ed1eb18722017e9b8d874a2378a3916
mateogall0 <mateogesede@gmail.com>
2025-07-25 14:56:16 -0300

    Add instructions on core models

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,1 +1,7 @@
+"""
+Core models must be agnostic and not use any external library like `Pydantic` or
+`annotated_types`.
+Constraints must be defined utilizing `dataclasses.field`, `attr`, `typing`,
+etc.
+"""
 from .base import Base, BaseEmbedded
