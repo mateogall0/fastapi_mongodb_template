ebe2f629230522919c80b431d3186ad33a4f8227
mateogall0 <mateogesede@gmail.com>
2025-07-30 09:46:08 -0300

    move: datetime field logic to infra layer

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,10 +1,9 @@
-from datetime import datetime, timezone
-from dataclasses import field
+from datetime import datetime
 from abc import ABC
 
 class Base(ABC):
-    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
-    updated_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime
+    updated_at: datetime
 
 class BaseEmbedded(ABC):
     pass
--- app/infra/db/models.py
+++ app/infra/db/models.py
@@ -1,5 +1,8 @@
 from beanie import Document
+from datetime import datetime, timezone
 from app.core.models import Base
+from pydantic import Field
 
 class BaseDoc(Base, Document):
-    pass
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
